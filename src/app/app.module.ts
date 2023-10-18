6409e9712b5e06313c36990406cc2b2fd4c69aef
System Administrator <root@macbook-pro-de-vandenbussche.home>
2023-10-18 16:13:33 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,25 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
+const firebaseConfig = { 
+  apiKey: "AIzaSyBlH3D78iYIMS4iQM9oK5pkKLweR8AYok8", 
+  authDomain: "ionic-03-e058d.firebaseapp.com", 
+  projectId: "ionic-03-e058d",
+  storageBucket: "ionic-03-e058d.appspot.com", 
+  messagingSenderId: "869233592769", 
+  appId: "1:869233592769:web:e1c32e1a09c06b24114740" 
+}; 
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule, 
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,68 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+  currentDate: string;
+  myTask: string;
+  addTask: boolean;
+  tasks: any;
+
+  constructor(public afDB: AngularFireDatabase) {
+    const dateFormat = Intl.DateTimeFormat();
+    const today = dateFormat.format(Date.now());
+    this.currentDate = today;
+    this.myTask = '';
+    this.addTask = false;
+    this.tasks = [];
+
+    this.getTasks();
+  }
+
+  addTaskToFirebase() {
+    console.log('myTask : ' + this.myTask);
+
+    this.afDB.list('Task/').push({
+      tache: this.myTask,
+      date: new Date().toISOString(),
+      termine: false
+    });
+    this.showForm();
+  }
+
+  showForm() {
+    this.addTask = !this.addTask;
+    this.myTask = '';
+  }
+
+  getTasks() {
+    this.afDB.list('Task/').snapshotChanges(['child_added', 'child_removed']).subscribe(actions => {
+      this.tasks = [];
+      actions.forEach(action => {
+        console.log('Tache : ' + action.payload.exportVal().tache + " " + action.payload.exportVal().date);
+        this.tasks.push({
+          key: action.key,
+          tache: action.payload.exportVal().tache,
+          date: action.payload.exportVal().date.substring(0, 10),
+          termine: action.payload.exportVal().termine
+        });
+      });
+    });
+  }
+
+  changeCheckState(ev: any) {
+    console.log('checked: ' + ev.termine);
+    this.afDB.object('Task/' + ev.key + '/termine/').set(ev.termine);
+  }
+
+  deleteTask(task: any) {
+    this.afDB.list('Task/').remove(task.key); 
+  }
 
 }
